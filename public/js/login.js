9c8dfd24fe7b63abf385bd6d8fce39c8afa55041
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-11-21 20:14:07 +1000

    Login and logout function in order to get working for landing page

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    
+    //Collect the values from the login form
+    const username = document.querySelector('#username-login').ariaValueMax.trim();
+    const password = document.querySelector('#password-login').ariaValueMax.trim();
+
+    if(username && password){
+        //Send a POST request to the API endpoint to check if the login is correct
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({username,password}),
+            headers: {'Content-Type':'application/json'},
+        });
+
+        if(response.ok){
+            //If the login is accepted, the browser will be directed to the the profile page
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
