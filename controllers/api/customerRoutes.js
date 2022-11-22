0e1193d3f8d9d7369ce3fbb5146d8d1d20aa2ba8
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-11-22 23:21:14 +1000

    update of customer, employee and user routes

--- controllers/api/customerRoutes.js
+++ controllers/api/customerRoutes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 const { User, Customers,Employees,TimeEntries,Jobs } = require('../../models');
-const { route } = require('./userRoutes');
+
 
     //find all customers for customers table
     //include all associated information from jobs and time entries
 router.get('/', async (req,res)=>{
     try{
         await Customers.findAll({
-            include:[
-                {
-                    model: Jobs,
-                },
-                {
-                    model: TimeEntries,
-                }
-            ]
-        }).then (data=> res.status(200).json(data))
+            // include:[
+            //     {
+            //         model: Jobs,
+            //     },
+            //     {
+            //         model: TimeEntries,
+            //     }
+            // ]
+        }).then ((data)=> res.status(200).json(data))
     } catch (err){
         res.status(500).json(err);
     }
@@ -23,21 +23,18 @@ router.get('/', async (req,res)=>{
 
     //find a single user by their customerID
     //include all associated information from jobs and time entries
-router.get('/:customerID',(req,res)=>{
+router.get('/:customerID', async (req,res)=>{
 
     try{
-        Customers.findOne({
-            where:{
-                customerID:req.params.customerID,
-            },
-            include:[
-                {
-                    model: Jobs,
-                },
-                {
-                    model:TimeEntries
-                }
-            ]
+         await Customers.findByPk(req.params.customerID, { 
+            // include:[
+            //     {
+            //         model: Jobs,
+            //     },
+            //     {
+            //         model:TimeEntries
+            //     }
+            // ]
         }).then(data=> res.status(200).json(data))
     } catch (err){
         res.status(500).json(err);
@@ -68,17 +65,40 @@ router.post('/', (req,res)=>{
 //       customer_contact_address: hobbiton
 //       customer_contact_email: peter@middleearth.com
 //       userID: -this will come from the associated user-
-//       customerID: -this will be auto iterating-
+//       customerID: -this will be auto iterating doesnt need to be entered -
 //     }
 //   */
     try{
-        const customerName = req.body
-        if(customerName){
-            Customers.create(customerName)
+        const newCustomer = req.body
+        if(newCustomer){
+            Customers.create(newCustomer)
             .then(data=> res.status(200).json(data));
         }
     }catch(err){
         res.status(400).json(err);
+    }
+});
+
+//update customer by their customerID
+router.put('/:customerID', (req,res)=>{
+    try{
+        Customers.update({
+            first_name: req.body.first_name,
+            last_name:req.body.last_name,
+            customer_contact_number:req.body.customer_contact_number,
+            customer_contact_address:req.body.customer_contact_address,
+            customer_contact_email:req.body.customer_contact_email,
+            userID:req.body.userID,
+        },
+        {
+            where:{
+                customerID:req.params.customerID
+            }
+        }).then ((data)=> {
+            res.status(200).json(data);
+        })
+    }catch(err){
+        res.status(500).json(err);
     }
 });
 
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const jobRoutes = require('./jobRoutes');
+const customerRoutes = require('./customerRoutes');
+const employeeRoutes = require('./employeeRoutes');
+const timeRoutes =require('./timeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/jobs',jobRoutes);
+router.use('/customers',customerRoutes);
+router.use('/employees', employeeRoutes);
+router.use('/time',timeRoutes);
 
 module.exports = router;
